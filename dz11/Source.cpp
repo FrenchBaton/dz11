620a6c87199268387e4b3d552d9e232316f265b6
hdgehog <hedgehogagaga@gmail.com>
2026-05-15 23:07:40 +0300

    commit

--- dz11/Source.cpp
+++ dz11/Source.cpp
@@ -1,11 +1,8 @@
 #include<iostream>
 using namespace std;
 
-//struct Point
-//{
-//	int x;
-//	int y;
-//};
+const char* PATH = "data.bin";
+const char* MODE = "ab+";
 
 struct Fraction
 {
@@ -13,36 +10,69 @@ struct Fraction
 	int denominator;
 };
 
-int main() {
-	const char* path = "data.bin";
-	const char* mode = "ab+";
+void writeFractionsCounts(unsigned int& n) {
+	do {
+		cout << "counts of fractions: ";
+		cin >> n;
+	} while (n == 0);
+}
 
-	unsigned int n = 1;
-	cout << "counts: ";
-	cin >> n;
-	//if(n > 0){}
-	Fraction* arr = new Fraction[n];
+Fraction* writeFractions(unsigned int& n) {
+	writeFractionsCounts(n);
+	Fraction* fr = new Fraction[n];
 	for (int i = 0; i < n; i++) {
 		cout << "numb: ";
-		cin >> arr[i].numberator;
+		cin >> fr[i].numberator;
 		cout << "denom: ";
-		cin >> arr[i].denominator;
+		cin >> fr[i].denominator;
 	}
+	return fr;
+}
 
-	FILE* file;
-	if (fopen_s(&file, path, mode) == 0) {
-		fwrite(&arr, sizeof(Fraction), n, file);
-		fseek(file, 0, SEEK_SET);
+void showFractions(Fraction*& arr, const unsigned int n) {
+	for (int i = 0; i < n; i++) {
+		cout << arr[i].numberator << "/" << arr[i].denominator << "\n";
+	}
+}
+
+void fileFuncShow(FILE*& file, Fraction*& arr, const unsigned int n) {
+	if (fopen_s(&file, PATH, MODE) == 0) {
 		Fraction* temp = new Fraction[n];
 		fread(&temp, sizeof(Fraction), n, file);
-		for (int i = 0; i < n; i++) {
-			cout << temp[i].numberator << "/" << temp[i].denominator << "\n";
-		}
+		showFractions(temp, n);
 		delete[] temp;
+	}
+}
+
+void fileFuncWrite(FILE*& file, Fraction*& arr, const unsigned int n) {
+	if (fopen_s(&file, PATH, MODE) == 0) {
+		fwrite(&arr, sizeof(Fraction), n, file);
+		fseek(file, 0, SEEK_SET);
+	}
+}
+
+void end(FILE*& file, Fraction*& arr) {
+	if (fopen_s(&file, PATH, MODE) == 0) {
 		fclose(file);
 	}
-	remove(path);
+	remove(PATH);
 	delete[] arr;
+}
+
+int main() {
+	FILE* file;
+	Fraction* arr;
+	unsigned int n = 0;
+
+	arr = writeFractions(n);
+	fileFuncWrite(file, arr, n);
+	cout << "\n";
+	fileFuncShow(file, arr, n);
+
+	showFractions(arr, n);
+	
+	
+	end(file, arr);
 
 	return 0;
 }
